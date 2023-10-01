990fbcc931a9b82020f1074efa11eb258980b9a0
nandanpi <nandanrp09@gmail.com>
2023-10-01 13:16:28 +0530

    feat: Layout changed in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-	import '../app.css';
+    import Navbar from "$lib/components/HeaderFooter/Navbar.svelte";
+    import Footer from "$lib/components/HeaderFooter/Footer.svelte";
+    import './styles.css';
 </script>
 
-<slot />
+<div class="app bg-secondary-light dark:bg-secondary-dark text-black dark:text-white duration-200">
+    <div>
+        <Navbar />
+    </div>
+    <main>
+        <slot />
+    </main>
+    <Footer />
+</div>
+
