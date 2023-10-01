9ff31e3b01c99ca853daa47bf26f639c303044fa
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-01 17:21:59 +0530

    refactor: replace svelte-icon-pack with iconify-icon
    Use a better icon pack

--- src/lib/components/HeaderFooter/Navbar.svelte
+++ src/lib/components/HeaderFooter/Navbar.svelte
@@ -1,43 +1,36 @@
 <script lang="ts">
-    import {NAVITEM} from "$lib/data/NavItems";
-    import ThemeToggle from "$lib/components/HeaderFooter/ThemeToggle.svelte";
-    import {page} from "$app/stores";
-    import NavbarMenu from "$lib/components/HeaderFooter/NavbarMenu.svelte";
-    import Icon from 'svelte-icons-pack/Icon.svelte';
-    import CgMenu from 'svelte-icons-pack/cg/CgMenu';
-    import {menuToggle} from "$lib/stores/menuToggle";
-    const toggleMenu = () => {
-        menuToggle.set(!$menuToggle); // Toggle the menu state
-    };
+	import { NAVITEM } from '$lib/data/NavItems';
+	import ThemeToggle from '$lib/components/HeaderFooter/ThemeToggle.svelte';
+	import { page } from '$app/stores';
+	import NavbarMenu from '$lib/components/HeaderFooter/NavbarMenu.svelte';
+	import { menuToggle } from '$lib/stores/menuToggle';
+	const toggleMenu = () => {
+		menuToggle.set(!$menuToggle); // Toggle the menu state
+	};
 
-    import { fade } from 'svelte/transition';
-
+	import { fade } from 'svelte/transition';
 </script>
 
-<section class="flex justify-between bg-primary-light dark:bg-primary-dark py-4 px-10 border-zinc-300 dark:border-zinc-800 items-center shadow-slate-100 dark:shadow-white">
-    <a href="/"><img src="/TASCLogo.png" alt="" class="w-20 h-7 hover:scale-110 duration-200"></a>
-    <ul class="md:flex space-x-10 hidden">
-        {#each NAVITEM as item}
-            <a href={`${item.href}`}>
-                <li class={`text-xl font-bold hover:dark:bg-primary-light hover:dark:text-black hover:bg-primary-dark hover:text-white px-3 py-2 rounded-xl hover:scale-110 duration-200 ${$page.url.pathname.split('/')[1] === item.title.toLowerCase() || ($page.url.pathname === '/' && item.title === 'Home') ? 'underline underline-offset-4' : ''}`}>
-                    <span>{item.title}</span>
-                </li>
-            </a>
-        {/each}
-    </ul>
-    <div class="items-center flex space-x-5">
-        <!--        <button class="bg-brand text-white px-4 py-2 rounded-xl hover:scale-110 duration-200 md:block hidden">-->
-        <!--            Login-->
-        <!--        </button>-->
-        <div ><ThemeToggle /></div>
-        <button class="md:hidden" on:click={toggleMenu}>
-            <Icon src={CgMenu} size="30"/>
-        </button>
-    </div>
+<section class="flex items-center justify-between border-zinc-300 bg-primary-light px-10 py-4 shadow-slate-100 dark:border-zinc-800 dark:bg-primary-dark dark:shadow-white">
+	<a href="/"><img src="/TASCLogo.png" alt="" class="h-7 w-20 duration-200 hover:scale-110" /></a>
+	<ul class="hidden space-x-10 md:flex">
+		{#each NAVITEM as item}
+			<a href={`${item.href}`}>
+				<li class={`rounded-xl px-3 py-2 text-xl font-bold duration-200 hover:scale-110 hover:bg-primary-dark hover:text-white hover:dark:bg-primary-light hover:dark:text-black ${$page.url.pathname.split('/')[1] === item.title.toLowerCase() || ($page.url.pathname === '/' && item.title === 'Home') ? 'underline underline-offset-4' : ''}`}>
+					<span>{item.title}</span>
+				</li>
+			</a>
+		{/each}
+	</ul>
+	<div class="flex items-center space-x-5">
+		<!-- <button class="hidden rounded-xl bg-brand px-4 py-2 text-white duration-200 hover:scale-110 md:block">Login</button> -->
+		<div><ThemeToggle /></div>
+		<button class="md:hidden" on:click={toggleMenu}>
+			<iconify-icon icon="ion:menu" height="2.5rem"></iconify-icon>
+		</button>
+	</div>
 
-
-
-    <div class={`md:hidden ${menuToggle? '' : 'hidden'} fixed z-50 top-0 right-0`} transition:fade={{ delay: 250, duration: 300 }}>
-        <NavbarMenu />
-    </div>
+	<div class={`md:hidden ${menuToggle ? '' : 'hidden'} fixed right-0 top-0 z-50`} transition:fade={{ delay: 250, duration: 300 }}>
+		<NavbarMenu />
+	</div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,25 @@
 <script>
-    import Icon from 'svelte-icons-pack/Icon.svelte';
-    import AboutSection from "$lib/components/Others/AboutSection.svelte";
-    import AiOutlineArrowDown from 'svelte-icons-pack/ai/AiOutlineArrowDown';
+	import AboutSection from '$lib/components/Others/AboutSection.svelte';
 </script>
 
 <div>
-    <div class="flex justify-center h-[80vh] items-center">
-        <div class={'space-y-6 px-4'}>
-            <h1
-                    class="font-extrabold text-transparent lg:text-8xl md:text-6xl text-[40px] bg-clip-text bg-gradient-to-r from-[#835FDC] to-[#CC4FAD] text-center"
-            >
-                Turing <br/> Artificial Intelligence <br/> Student Committee
-            </h1>
+	<div class="flex h-[80vh] items-center justify-center">
+		<div class={'space-y-6 px-4'}>
+			<h1 class="bg-gradient-to-r from-[#835FDC] to-[#CC4FAD] bg-clip-text text-center text-[40px] font-extrabold text-transparent md:text-6xl lg:text-8xl">
+				Turing <br /> Artificial Intelligence <br /> Student Committee
+			</h1>
 
-            <p class="lg:text-2xl md:text-lg text-smA font-semibold text-center">
-                Department of Artificial Intelligence and Machine Learning
-            </p>
-        </div>
-    </div>
+			<p class="text-smA text-center font-semibold md:text-lg lg:text-2xl">Department of Artificial Intelligence and Machine Learning</p>
+		</div>
+	</div>
 
-    <a href="/#about">
-        <div class={'animate-bounce flex justify-center pb-16'}>
-            <Icon src={AiOutlineArrowDown} size="60" color="#5E20AB"/>
-        </div>
-    </a>
+	<a href="/#about">
+		<div class={'flex animate-bounce justify-center pb-16'}>
+			<iconify-icon icon="mingcute:arrow-up-fill" rotate="180deg" height="4rem"></iconify-icon>
+		</div>
+	</a>
 
-    <div id="about" class="py-10">
-        <AboutSection />
-    </div>
+	<div id="about" class="py-10">
+		<AboutSection />
+	</div>
 </div>
