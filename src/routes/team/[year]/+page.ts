b60c62c16fb46d3a5c8efcc34c54298a0a3d2311
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-10-02 11:52:52 +0530

    feat: add end to end type safety everywhere

--- src/routes/team/[year]/+page.ts
+++ src/routes/team/[year]/+page.ts
@@ -1,13 +1,13 @@
 import { db } from '$lib/firebase/firebase';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, type DocumentData } from 'firebase/firestore';
+import type MemberCardType from '$lib/components/types/MemberCardType';
 import type { PageLoad } from './$types';
 
-// @ts-ignore
-export const load = (async ({params}) => {
-    const postSnapshot = await getDocs(collection(db, 'members',params.year,'post'));
-    const Data = [];
-    for (const doc of postSnapshot.docs) {
-        Data.push(doc.data());
-    }
-    return {Data};
-})satisfies PageLoad;
+export const load = (async ({ params }) => {
+	const postSnapshot = await getDocs(collection(db, 'members', params.year, 'post'));
+	const Data: DocumentData = [];
+	for (const doc of postSnapshot.docs) {
+		Data.push(doc.data());
+	}
+	return { Data: Data as MemberCardType['Data'] };
+}) satisfies PageLoad;
