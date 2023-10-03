72a57b3e52be71d5fac99000c048fe24efd21f9b
prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-03 11:37:56 +0530

    style: update color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,30 @@ export default {
 		extend: {
 			colors: {
 				brand: '#5E20AB',
-				'primary-dark': '#000015',
-				'primary-light': '#ECCAFF',
-				'secondary-dark': '#000029',
-				'secondary-light': '#F3E1FF',
-				'muted-dark': '#121928',
+
+				// Used in NavBar
+				'primary-dark': '#000000',
+				'primary-light': '#ECCAFF', // '#F7ECFF',
+
+				// Used as background
+				'secondary-dark': '#090909',
+				'secondary-light': '#FFFFFF',
+
+				'muted-dark': '#121212',
 				'muted-light': '#F3E7FB',
+
+				// Used in boxes
+				'accent-light': '#ECCAFF',
+				'accent-dark': '#101F1F',
+
+				// Nandan's theme
+				// 'primary-dark': '#000015',
+				// 'primary-light': '#ECCAFF',
+				// 'secondary-dark': '#000029',
+				// 'secondary-light': '#F3E1FF',
+				// 'muted-dark': '#121928',
+				// 'muted-light': '#F3E7FB',
+
 				white: '#FFFFFF',
 				black: '#000000'
 			}
