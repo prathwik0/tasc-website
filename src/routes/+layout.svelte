8165f3a232799e42d6062bc8a7b19c5d02cc44bd
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-06 21:40:50 +0530

    style: update root layout, navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script>
-    import Navbar from "$lib/components/HeaderFooter/Navbar.svelte";
-    import Footer from "$lib/components/HeaderFooter/Footer.svelte";
-    import '../app.css';
+	import '../app.css';
+
+	import Navbar from '$lib/components/HeaderFooter/Navbar.svelte';
+	import Footer from '$lib/components/HeaderFooter/Footer.svelte';
+
+	let y = 0;
 </script>
 
-<div class="app bg-secondary-light dark:bg-secondary-dark text-black dark:text-white duration-200">
-    <div>
-        <Navbar />
-    </div>
-    <main>
-        <slot />
-    </main>
-    <Footer />
+<svelte:window bind:scrollY={y} />
+
+<div class="sticky top-0 z-50 w-full" class:bg-navbar={y >= 20}>
+	<Navbar />
 </div>
 
+<slot />
+
+<Footer />
