93591d20de1fb53feb07594927469320f1a3ae26
prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-07 05:00:08 +0530

    style: update handbook
    - switch from url parameters to query string
    - fix iframe size issues

---
+++ src/routes/handbook/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+	let pageNumber = $page.url.searchParams.get('page') ?? 1;
+</script>
+
+<div class="flex max-h-screen flex-grow">
+	<iframe class="aspect-video flex-grow md:pb-12" title="handbook" src="https://heyzine.com/flip-book/d49adcee6e.html#page/{pageNumber}" />
+</div>
--- src/routes/handbook/[pageNumber]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-	const pageNumber = $page.params.pageNumber;
-</script>
-
-<div class="flex flex-grow">
-	<iframe class="aspect-video flex-grow" title="handbook" src="https://heyzine.com/flip-book/d49adcee6e.html#page/${pageNumber}" />
-</div>
