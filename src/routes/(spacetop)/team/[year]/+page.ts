c5a9c3b2635439be8605b8434c78571c2f15f3ab
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-08 21:13:14 +0530

    refactor: rename firebase.ts to firebase_static.ts
    firebase_static.ts will hold the config of firebase app that has the static assets for now

--- src/routes/(spacetop)/team/[year]/+page.ts
+++ src/routes/(spacetop)/team/[year]/+page.ts
@@ -1,4 +1,4 @@
-import { db } from '$lib/firebase/firebase';
+import { db } from '$lib/firebase/firebase_static';
 import { collection, getDocs, type DocumentData } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 import type MemberData from '$lib/components/types/MemberData';
