c497ebf124cb53c1a44bbae825b813829a164b9e
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-19 13:43:59 +0530

    refactor: move files to (navbar) folder

--- src/routes/(navbar)/+page.svelte
+++ src/routes/(navbar)/+page.svelte
@@ -4,7 +4,7 @@
 
 <div>
 	<div class="h-screen">
-		<div class="flex h-[80vh] items-center justify-center">
+		<div class="flex h-[90vh] items-center justify-center">
 			<div class="space-y-6 px-4">
 				<h1 class="bg-gradient-to-r from-[#835FDC] to-[#CC4FAD] bg-clip-text text-center text-[40px] font-extrabold text-transparent md:text-6xl lg:text-8xl">
 					Turing <br /> Artificial Intelligence <br /> Student Committee
