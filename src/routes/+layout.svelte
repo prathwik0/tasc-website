450815d449829e4dbd5d834ed60e4891de677e21
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-20 22:12:11 +0530

    chore: add userLoaded and called stores to the root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import { darkTheme } from '$lib/stores/theme';
-	import { user, userData } from '$lib/firebase/firebase';
+	import { user, userData, userLoaded, called } from '$lib/firebase/firebase';
 
 	/* Subscribe to stores in root layout */
 	$darkTheme;
 	$user;
 	$userData;
+	$userLoaded;
+	$called;
 </script>
 
 <slot />
