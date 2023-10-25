d405cf63c7deb5b72cc4e0b2cdebb7625b702332
prathwik <64214685+prathwik0@users.noreply.github.com>
2023-10-25 23:04:04 +0530

    fix: routing to /edit page

--- src/lib/components/Auth/EditButton.svelte
+++ src/lib/components/Auth/EditButton.svelte
@@ -7,5 +7,5 @@
 {#if !$userLoaded}
 	<span></span>
 {:else if $user && $userData && $userData.username === currentUsername}
-	<a href="/{$userData.username}" class="text-center"><Button variant="outline">Edit Profile</Button></a>
+	<a href="/{$userData.username}/edit" class="text-center"><Button variant="outline">Edit Profile</Button></a>
 {/if}
