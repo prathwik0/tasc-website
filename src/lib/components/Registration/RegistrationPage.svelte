f4c65fb08cfe8d708d6650ba51f25306d95780ff
nandanpi <nandanrp09@gmail.com>
2023-10-26 18:50:21 +0530

    bug: Fields and content updated

--- src/lib/components/Registration/RegistrationPage.svelte
+++ src/lib/components/Registration/RegistrationPage.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input';
-	import { storage } from '$lib/firebase/firebase';
-	import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-	import { Progress } from '$lib/components/ui/progress';
-
-	let previewURL: string;
-	let uploading = false;
-	let teamName: string;
-
-	async function upload(e: any) {
-		uploading = true;
-		const file = e.target.files[0];
-		previewURL = URL.createObjectURL(file);
-
-		const storageRef = ref(storage, `proofOfPayment/${teamName}/proof.png`);
-		const result = await uploadBytes(storageRef, file);
-
-		const url = await getDownloadURL(result.ref);
-
-		alert('Uploaded!');
-		uploading = false;
-	}
 </script>
 
 <div class="pt-4">
-	<h1 class="text-center text-5xl font-bold text-brand md:text-7xl">ESports Mayhem</h1>
+	<h1 class="text-center text-5xl font-bold text-brand md:text-7xl">E-Sports Mayhem</h1>
 	<div class="mx-10 flex items-center justify-center py-10">
 		<div class="w-full max-w-[100rem] space-y-6 rounded-xl bg-gray-300 p-6 dark:bg-muted-dark">
 			<div class="flex flex-col items-center space-y-6 md:flex-row md:space-y-0">
@@ -33,8 +12,8 @@
 				</div>
 
 				<div class="flex w-full flex-col justify-center space-y-6 md:max-w-7xl">
-					<p class="text-lg md:text-xl lg:text-2xl">Dear Participants, Introducing "ESPORTS MAYHEM," an exciting esports event where you can flaunt your gaming skills in Valorant, BGMI (Battlegrounds Mobile India), and COD Mobile (Call of Duty Mobile)</p>
-					<div class="space-y-2 text-lg md:text-2xl">
+					<p class="text-md md:text-lg lg:text-xl">Dear Participants, introducing "E-SPORTS MAYHEM," an exciting esports event where you can flaunt your gaming skills in Valorant, BGMI (Battlegrounds Mobile India), and COD Mobile (Call of Duty Mobile)</p>
+					<div class="text-md space-y-2 md:text-lg lg:text-xl">
 						<p>Number of Teammates:</p>
 						<ul>
 							<li>Valorant: 5</li>
@@ -42,7 +21,7 @@
 							<li>COD Mobile: 5</li>
 						</ul>
 					</div>
-					<div class="space-y-2 text-lg md:text-2xl">
+					<div class="text-md space-y-2 md:text-lg lg:text-xl">
 						<p>Entry fee per team:</p>
 						<ul>
 							<li>Valorant: 250 INR</li>
@@ -50,7 +29,12 @@
 							<li>COD Mobile: 150 INR</li>
 						</ul>
 					</div>
-					<p class="text-lg md:text-2xl">To participate, simply fill out the form below and let the games begin! (TEAM LEADER ONLY)</p>
+					<p class="text-md md:text-lg lg:text-xl">To participate, simply fill out the form below and let the games begin! (TEAM LEADER ONLY)</p>
+					<div class="text-md md:text-lg lg:text-xl">
+						<p class="font-bold text-red-600">NOTE:</p>
+						<p>1. Entry fee will be collected at venue.</p>
+						<p>2. Details of the teammates will also be collected there.</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -62,6 +46,7 @@
 		<div class=" mx-10 flex items-center justify-center py-10">
 			<div class="w-full max-w-[100rem] space-y-6 rounded-xl bg-gray-300 p-6 dark:bg-muted-dark">
 				<form action="https://docs.google.com/forms/d/e/1FAIpQLSdB47YWfENsSlr6c-00g1OOjX9m25_6sXeiID9MmE2K4peOyg/formResponse" method="POST" enctype="multipart/form-data">
+					<p class="pb-4 font-semibold text-red-600">Only team leader has to fill the form!</p>
 					<div class="grid-column-1 grid w-full max-w-[100rem] space-y-6 text-black dark:text-white">
 						<section class="space-y-4">
 							<label for="name">Name*</label>
@@ -114,18 +99,9 @@
 						</section>
 
 						<section class="space-y-4">
-							<label for="proof">Team name*</label>
-							<Input id="proof" type="text" name="entry.1257526011" placeholder="Team Name" class="bg-transparent font-semibold text-brand" bind:value={teamName} required />
+							<label for="team">Team name*</label>
+							<Input id="team" type="text" name="entry.1257526011" placeholder="Team Name" class="bg-transparent font-semibold text-brand" required />
 						</section>
-
-						<section class="space-y-4">
-							<label for="proof">Upload proof of payment* <br /> Pay to : brijwinbalakrishna@oksbi</label>
-							<Input on:change={upload} name="photoURL" type="file" accept="image/*" id="proof" required class="font-semibold text-brand" />
-						</section>
-						{#if uploading}
-							<p>Uploading...</p>
-							<Progress value={33} />
-						{/if}
 					</div>
 
 					<div class="flex w-full justify-center">
