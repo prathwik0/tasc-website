7b0c9b2d301c6db48b7c02a048cc6c702da86650
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-06 16:43:36 +0530

    feat: custom global css for hackathon page
    added to body using sveltekit actions

--- src/lib/actions/style.ts
+++ src/lib/actions/style.ts
@@ -14,10 +14,31 @@ export function backgroundColor(node: any, color: { color_light: string; color_d
 	};
 }
 
-
 /*
  * todo: fix the race condition
  * currently while navigating to a new page, the background color is set
  * before the destroy method is called on the previous page
  * hence. the background color is set to default
  */
+/* Fixed by kausthubh by adding loading spinners */
+
+/** @type {import('svelte/action').Action<HTMLElement, string>}  */
+export function hackathonStyles(node: any) {
+	// console.log('created');
+	node.classList.add('hackathon');
+	node.style['background-color'] = '#020817';
+	node.style['color'] = 'white';
+	node.style['scroll-behaviour'] = 'smooth';
+	node.style['scroll-padding-top'] = '5rem';
+
+	return {
+		destroy() {
+			// console.log('destroyed');
+			node.classList.remove('hackathon');
+			node.style['background-color'] = '';
+			node.style['color'] = '';
+			node.style['scroll-behaviour'] = '';
+			node.style['scroll-padding-top'] = '';
+		}
+	};
+}
