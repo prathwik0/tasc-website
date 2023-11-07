34d65c77c841d9313937c15906eae2467243c3b6
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-07 07:14:48 +0530

    feat: Create muted and main button components
    Co-Authored-By: Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>

---
+++ src/lib/components/Hackathon/MutedButton.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let hrefId = '/';
+</script>
+
+<div class="my-5 flex flex-col items-center justify-center">
+	<div class="rounded-2xl bg-gray-600 p-0.5 font-jbMedium">
+		<div class="rounded-2xl bg-[#020817] py-3 underline-offset-8">
+			<div class=" transition duration-300">
+				<a href={hrefId} class=" px-5 py-5 underline-offset-8 transition duration-300"> <slot /> </a>
+			</div>
+		</div>
+	</div>
+</div>
