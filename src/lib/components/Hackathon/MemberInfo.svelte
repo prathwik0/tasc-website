b77d0ea3cbc6ce15a4d585caa9be57d24254f3f6
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-07 07:16:58 +0530

    feat: Create MemberInfo component
    Co-Authored-By: Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>

---
+++ src/lib/components/Hackathon/MemberInfo.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { user, userLoaded, userData } from '$lib/firebase/firebase';
+	import MainButton from './MainButton.svelte';
+	import MutedButton from './MutedButton.svelte';
+
+	import RegisterButton from './RegisterButton.svelte';
+</script>
+
+{#if !$userLoaded}
+	<span></span>
+{:else if $user && $userData}
+	<p class="pt-6 text-2xl">Hello {$userData.name}!</p>
+	<div class="flex gap-2">
+		<MainButton hrefId="#">Create Team (soon...)</MainButton>
+		<MutedButton hrefId="#">Join Team (soon...)</MutedButton>
+	</div>
+{:else}
+	<RegisterButton />
+{/if}
