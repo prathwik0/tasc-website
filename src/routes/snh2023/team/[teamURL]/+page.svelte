5ae02831c699d5291ea60764da67bec21b237304
nandanpi <nandanrp09@gmail.com>
2023-11-09 13:35:59 +0530

    chore: UI Improvements

--- src/routes/snh2023/team/[teamURL]/+page.svelte
+++ src/routes/snh2023/team/[teamURL]/+page.svelte
@@ -9,13 +9,13 @@
 
 <!-- Not in a team -->
 <div class="flex h-full min-h-screen w-full flex-col items-center justify-center space-y-6">
-	<h2 class="pt-4 font-jbExtrabold text-4xl">Your Team</h2>
+	<h2 class="pt-4 font-jbExtrabold text-3xl md:text-4xl">Your Team</h2>
 
-	<div class="gradient_border mx-5 w-auto cursor-default rounded-2xl bg-[#d1f2f9] bg-opacity-5 p-10 backdrop-blur-sm transition duration-300 md:w-[600px]">
+	<div class="text-md mx-5 w-[23rem] cursor-default rounded-2xl border-[1px] border-[#d2b863] bg-gray-400 bg-opacity-5 px-5 py-2 backdrop-blur-sm transition duration-300 md:w-[35rem] md:p-10 md:text-2xl">
 		<ul class="mt-5 space-y-2">
-			<p class="font-jbRegular text-lg sm:text-2xl">Team Name : <span class="text-[#fffba4]">{data.teamName}</span></p>
-			<p class="flex items-center space-x-4 font-jbRegular text-lg sm:text-2xl">
-				Team ID :
+			<p class="font-jbRegular">Team Name: <span class="text-[#fffba4]">{data.teamName}</span></p>
+			<p class="flex items-center space-x-2 font-jbRegular">
+				Team ID:
 				<span class="pl-3 text-[#fffba4]">{data.teamURL}</span>
 
 				<button class="rounded-x rounded-lg bg-gray-600 p-1" use:copy={data.teamURL} on:svelte-copy={() => alert('Team ID copied to clipboard!')}>
@@ -30,8 +30,8 @@
 				</button>
 			</p>
 
-			<p class="font-jbRegular text-lg sm:text-2xl">
-				Team Secret :
+			<p class="font-jbRegular">
+				Team Secret:
 				<span class="text-[#fffba4]">{data.teamSecret}</span>
 				<button class="rounded-x rounded-lg bg-gray-600 p-1" use:copy={data.teamSecret} on:svelte-copy={() => alert('Team Secret copied to clipboard!')}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
@@ -45,13 +45,13 @@
 				</button>
 			</p>
 
-			<h2 class="bg-gradient-to-tr pt-4 font-jbExtrabold text-2xl drop-shadow-[0_0_0.15rem_#d2b863]">Team Members</h2>
+			<h2 class="pt-4 font-jbExtrabold">Team Members</h2>
 			<div class="flex flex-col space-y-2">
 				{#each data.memberInfo as memberInfo}
 					{#if memberInfo.id == data.leader}
-						<p class="font-jbRegular text-lg text-[#fffba4] sm:text-2xl">👑 {memberInfo.name}</p>
+						<p class="font-jbRegular text-[#fffba4]">👑 {memberInfo.name}</p>
 					{:else}
-						<p class="font-jbRegular text-lg text-[#fffba4] sm:text-2xl">♟️ {memberInfo.name}</p>
+						<p class="font-jbRegular text-[#fffba4]">♟️ {memberInfo.name}</p>
 					{/if}
 				{/each}
 			</div>
@@ -64,6 +64,6 @@
 		</ul>
 	</div>
 
-	<MainButton>Solution Submission (soon...)</MainButton>
 	<LeaveTeam teamID={data.teamURL} memberInfo={data.memberInfo} leader={data.leader} />
+	<MainButton>Solution Submission (soon...)</MainButton>
 </div>
