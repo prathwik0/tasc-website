7437617a0093b6bccac2cb6a899d43421a423e3b
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-11 19:50:56 +0530

    chore: increase the number of colleges

--- src/lib/components/Hackathon/Submission.svelte
+++ src/lib/components/Hackathon/Submission.svelte
@@ -4,7 +4,7 @@
 	import { db, user, userData, userID, userProfileData } from '$lib/firebase/firebase';
 	import { doc, collection, getDoc, setDoc, writeBatch, limit, query, where, updateDoc, arrayUnion, increment } from 'firebase/firestore';
 
-	const collegeList = ['NMAMIT, Nitte', 'Sahyadri, Mangalore', 'NITK, Surathkal', 'Other'];
+	const collegeList = ['NMAMIT, Nitte', 'Sahyadri, Mangalore', 'NITK, Surathkal', 'MAHE, Manipal', 'St Joseph Engineering College, Mangalore', "Alva's College, Moodbidri", 'Yenepoya College, Mangalore', 'Other'];
 	const PList = ['SNH01', 'SNH02', 'SNH03', 'SNH04', 'SNH05', 'SNH06', 'SNH07'];
 
 	export let teamID: string;
