bcfedbd2fccd620801e0a3011941c9973347867d
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-16 13:19:17 +0530

    feat: datafetching for certificates

---
+++ src/routes/certificate/[certificateID]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Certificate | TASC</title>
+	<meta name="description" content={data.occasion} />
+</svelte:head>
+
+<div>
+	{data.certificateID}
+	{data.date}
+	{data.name}
+	{data.teamName}
+	{data.user}
+	{data.team}
+	{data.role}
+	{data.occasion}
+</div>
