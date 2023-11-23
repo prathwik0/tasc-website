b9a32de663ed581d371c1232382c69328a1160a3
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-23 17:28:41 +0530

    enhance: query using either room and floor

--- src/routes/admin/snh2023final/+page.svelte
+++ src/routes/admin/snh2023final/+page.svelte
@@ -10,12 +10,15 @@
 	import type { TeamDataSNH2023 } from '$lib/components/types/TeamData';
 
 	let data: TeamDataSNH2023[] = [];
-	let floor: number;
+	let floor: number = 1;
+	let room: number = 1;
 
 	async function getData() {
 		const teamsRef = collection(db, 'snh2023final');
 		let q;
-		if (floor) {
+		if (floor && room) {
+			q = query(teamsRef, where('floor', '==', floor), where('room', '==', room));
+		} else if (floor) {
 			q = query(teamsRef, where('floor', '==', floor));
 		} else {
 			q = query(teamsRef);
@@ -59,11 +62,18 @@
 
 		alert('Room Updated');
 	}
+
+	function getDate(timestamp: { seconds: number }) {
+		return Timestamp.fromMillis(timestamp.seconds * 1000)
+			.toDate()
+			.toLocaleString();
+	}
 </script>
 
 <div class="flex flex-col items-center justify-center text-center">
 	<div class="flex w-full max-w-sm items-center space-x-2">
 		<Input bind:value={floor} type="number" placeholder="Enter Floor Number" />
+		<Input bind:value={room} type="number" placeholder="Enter Room Number" />
 		<Button on:click={getDataWrapper}>Query the Database</Button>
 	</div>
 
@@ -79,7 +89,6 @@
 				<Table.Head class="text-center">PID</Table.Head>
 				<Table.Head class="max-w-[5rem] text-center">Link</Table.Head>
 				<Table.Head>Leader Name</Table.Head>
-				<Table.Head>Leader Phone</Table.Head>
 				<Table.Head class="text-center">Leader Email</Table.Head>
 				<Table.Head class="text-center">Team ID</Table.Head>
 				<Table.Head class="text-center">Team Secret</Table.Head>
@@ -109,9 +118,8 @@
 					<Table.Cell class="text-wrap max-w-[5rem]"><a href={team.submission.link} class="text-blue-600" target="_blank">Click Here</a></Table.Cell>
 
 					<Table.Cell>{team.leaderName}</Table.Cell>
-					<Table.Cell>{team.leaderPhone}</Table.Cell>
 					<Table.Cell>{team.leaderEmail}</Table.Cell>
-					<Table.Cell>{team.teamURL}</Table.Cell>
+					<Table.Cell>{team.teamID}</Table.Cell>
 					<Table.Cell>{team.teamSecret}</Table.Cell>
 
 					<Table.Cell class="text-center"><input type="text" bind:value={data[i].floor} /></Table.Cell>
@@ -137,7 +145,7 @@
 						{#if team.members[i]}
 							<Table.Cell>{team[team.members[i]].name}</Table.Cell>
 							<Table.Cell>{team[team.members[i]].status}</Table.Cell>
-							<Table.Cell>{team[team.members[i]].timestamp}</Table.Cell>
+							<Table.Cell>{getDate(team[team.members[i]].timestamp)}</Table.Cell>
 							<Table.Cell>{team[team.members[i]].phone}</Table.Cell>
 						{:else}
 							<Table.Cell>Na</Table.Cell>
