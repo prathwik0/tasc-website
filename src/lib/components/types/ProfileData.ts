e8a2781be176f1c74b8a250b64da1f3edb048f73
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-26 03:26:23 +0530

    enhance: update ProfileData.ts

--- src/lib/components/types/ProfileData.ts
+++ src/lib/components/types/ProfileData.ts
@@ -4,13 +4,41 @@ interface Link {
 	title: string;
 }
 
-export default interface ProfileData {
-	published: boolean;
-	username?: string;
-	name?: string;
+interface ImmutableData {
+	username: string;
+	name: string;
+	email: string;
+	achievements: string[];
+	[dynamicField: string]: unknown;
+}
+
+interface Achievement {
+	name: string;
+	description: string;
+	certificateTitle: string;
+	certificateURL: string;
+	certificateID: string;
+
+	issueDate: string;
+	organization: string;
+
+	occassion: string;
+	role: string;
+
+	event?: string;
+	teamID?: string;
+	teamName?: string;
+}
+
+type DynamicFields<T> = {
+	[K in keyof T as `${string & K}`]: Achievement;
+};
+
+interface MutableData {
 	usn?: string;
 	phone?: string;
 	college?: string;
+	branch?: string;
 	photoURL: string;
 	bio: string;
 	color_light: string;
@@ -19,3 +47,6 @@ export default interface ProfileData {
 	snh2023: string;
 	snh2023final: string;
 }
+
+// Intersection of TeamData and DynamicHistoryFields to enforce the structure
+export type ProfileData = ImmutableData & DynamicFields<ImmutableData> & MutableData;
