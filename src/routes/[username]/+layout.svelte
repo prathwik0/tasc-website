4a718eb384d051d94d3660eb1e70e24fb3ed6768
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-11-29 22:24:01 +0530

    fix: minor fixes

--- src/routes/[username]/+layout.svelte
+++ src/routes/[username]/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import AuthButton from '$lib/components/Auth/AuthButton.svelte';
+	import ThemeToggle from '$lib/components/HeaderFooter/ThemeToggle.svelte';
 	import Vectorall from '$lib/components/VectorBackground/vectorall.svelte';
 </script>
 
@@ -8,6 +9,7 @@
 	<div class="flex h-full items-center justify-between px-10">
 		<a href="/"><img src="/TASCLogo.png" alt="TASC logo" class="w-20" /></a>
 		<AuthButton />
+		<ThemeToggle/>
 	</div>
 </nav>
 
