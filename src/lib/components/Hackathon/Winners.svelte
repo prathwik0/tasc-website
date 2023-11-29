e8aaceb27784c3e1407243fc5c48d372f623ec26
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-11-29 08:07:00 +0530

    feat: winners page snh2023

---
+++ src/lib/components/Hackathon/Winners.svelte
@@ -0,0 +1,33 @@
+<div class="mx-4 flex h-full flex-col items-center justify-center">
+	<h1 class="z-40 text-center font-jbExtrabold text-2xl md:text-4xl">Winners - Sapient Scholars</h1>
+	<div class="text-md z-40 pb-5 text-center font-jbExtrabold md:text-lg">Dayananda Sagar University, Bangalore</div>
+	<img class="mb-8 md:px-20 lg:px-48" src="/snh2023/winners.png" alt="Winners of SNH2023" />
+</div>
+<div class="mx-4 mt-10 flex h-full flex-col items-center justify-center">
+	<h1 class="z-40 text-center font-jbExtrabold text-2xl md:text-4xl">Runners Up - PrepAir</h1>
+	<div class="text-md z-40 pb-5 text-center font-jbExtrabold md:text-lg">SMVITM, Udupi</div>
+	<img class="mb-8 md:px-20 lg:px-48" src="/snh2023/runners.png" alt="Runners Up of SNH2023" />
+</div>
+<div class="mx-4 mt-10 flex h-full flex-col items-center justify-center">
+	<h1 class="z-40 text-center font-jbExtrabold text-2xl md:text-4xl">Second Runners Up - Black Perl</h1>
+	<div class="text-md z-40 pb-5 text-center font-jbExtrabold md:text-lg">NMAMIT, Nitte</div>
+	<img class="mb-8 md:px-20 lg:px-48" src="/snh2023/secondRunners.png" alt="Second Runners Up of SNH2023" />
+</div>
+<div class="mx-10 mb-24 mt-10 flex justify-center">
+	<div class="flex flex-col">
+		<h1 class="z-40 text-center font-jbExtrabold text-2xl md:text-4xl">Internships Provided to</h1>
+		<ol class="list-decimal">
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Black Perl from NMAMIT, Nitte</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">PrepAir from Shri Madhwa Vadiraja Institute of Technology & Management</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Sapient Scholars from Dayananda Sagar University, Bangalore</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Over Loaded from NMAMIT, Nitte</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Caffeinated Coders from St Joseph Engineering College, Mangalore</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">ERROR 404 from Shri Madhwa Vadhiraja Institute of Technology & Mangagement</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Nexxus from Nitte Institute of Professional Education</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Cosmos from Sahyadri, Mangalore</li>
+			<li class="text-md z-40 pt-2 font-jbExtrabold md:text-lg lg:text-xl">Vegshell from AJ Institute of Engineering and Technology</li>
+			<!-- <div class="z-40 pt-2 font-jbExtrabold text-lg md:text-xl">10. asdf</div>
+	<div class="z-40 pt-2 font-jbExtrabold text-lg md:text-xl">11. asdf</div> -->
+		</ol>
+	</div>
+</div>
