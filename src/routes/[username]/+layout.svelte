33af662ea295ea7d8fe2bb0ce99a2846a1b564b7
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-11-29 21:59:38 +0530

    refactor: move username page

---
+++ src/routes/[username]/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import AuthButton from '$lib/components/Auth/AuthButton.svelte';
+	import Vectorall from '$lib/components/VectorBackground/vectorall.svelte';
+</script>
+
+<Vectorall />
+<nav class="block h-20 w-full">
+	<div class="flex h-full items-center justify-between px-10">
+		<a href="/"><img src="/TASCLogo.png" alt="TASC logo" class="w-20" /></a>
+		<AuthButton />
+	</div>
+</nav>
+
+<slot />
