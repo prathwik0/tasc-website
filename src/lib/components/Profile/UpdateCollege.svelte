9220b8a80237f4f99769f3e8f8f060ab55b6b86f
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-12-02 21:01:40 +0530

    chore: college component update

--- src/lib/components/Profile/UpdateCollege.svelte
+++ src/lib/components/Profile/UpdateCollege.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { doc, updateDoc } from 'firebase/firestore';
 	import { db, userID, userProfileData } from '$lib/firebase/firebase';
+	import { doc, updateDoc } from 'firebase/firestore';
 	import Button from '../ui/custom_button/button.svelte';
-	import Label from '../ui/label/label.svelte';
 	import Input from '../ui/input/input.svelte';
+	import Label from '../ui/label/label.svelte';
 
 	let college = '';
 
@@ -28,12 +28,11 @@
 	}
 </script>
 
-<div class="w-full py-2">
-	<Label>Current College: <span>{$userProfileData?.college}</span></Label>
+<h1 class="text-2xl font-medium">Update your College</h1>
+
+<div class="w-full py-1">
+	<Label class="text-base text-slate-500">{$userProfileData?.college}</Label>
 </div>
 
-<div class="grid gap-1.5">
-	<Label for="college">Update college name</Label>
-	<Input name="college" placeholder="Enter your college name here" bind:value={college} />
-</div>
-<Button class="mt-4" on:click={updateCollege}>Update College Name</Button>
+<Input name="college" placeholder="Enter your college name here" class="border-slate-400 focus:border-white" bind:value={college} />
+<Button class="mt-4 w-1/3 min-w-fit self-center border transition-all duration-300 ease-in-out hover:bg-[#020817]" variant="ghost" on:click={updateCollege}>Update College</Button>
