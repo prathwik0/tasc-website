2ebaf7e1a4aa96279395b6ae70b816cb698415f2
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-12-03 22:10:50 +0530

    chore: ux and fixes

--- src/lib/components/Profile/UpdateCollege.svelte
+++ src/lib/components/Profile/UpdateCollege.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { db, userID, userProfileData } from '$lib/firebase/firebase';
 	import { doc, updateDoc } from 'firebase/firestore';
+	import { exclaim, success } from '../Toast/toast';
 	import Button from '../ui/custom_button/button.svelte';
 	import Input from '../ui/input/input.svelte';
 	import Label from '../ui/label/label.svelte';
@@ -14,7 +15,7 @@
 
 	async function updateCollege(e: Event) {
 		if (!isValidCollege) {
-			alert('Please use only the specified set of characters - A-Za-z0-9~!#()[]{}",._ and make sure that you have entered 4 to 40 characters');
+			exclaim('Please use only the specified set of characters - A-Za-z0-9~!#()[]{}",._ and make sure that you have entered 4 to 40 characters');
 			return;
 		}
 
@@ -25,14 +26,15 @@
 		await updateDoc(userRef, {
 			college
 		});
+		success('Updated College successfully');
 	}
 </script>
 
 <h1 class="text-2xl font-medium">Update your College</h1>
 
 <div class="w-full py-1">
-	<Label class="text-base text-slate-500">{$userProfileData?.college}</Label>
+	<Label class="text-base text-muted">{$userProfileData?.college}</Label>
 </div>
 
-<Input name="college" placeholder="Enter your college name here" class="border-slate-400 focus:border-white" bind:value={college} />
-<Button class="mt-4 w-1/3 min-w-fit self-center border transition-all duration-300 ease-in-out hover:bg-[#020817]" variant="ghost" on:click={updateCollege}>Update College</Button>
+<Input name="college" placeholder="Enter your college name here" class="border-muted focus:border-primary" bind:value={college} />
+<Button class="mt-4 w-1/3 min-w-fit self-center border hover:bg-background" variant="ghost" on:click={updateCollege}>Update College</Button>
