2b367553555a07e223021430561afe2dac2b2dfa
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-12-03 22:09:03 +0530

    feat: bg vector components

---
+++ src/lib/components/VectorBackground/vectorTR.svelte
@@ -0,0 +1,36 @@
+	<div class="absolute right-0 top-0 z-0 hidden md:block">
+		<svg width="307" height="273" viewBox="0 0 307 273" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g opacity="0.7">
+				<g opacity="0.7">
+					<path opacity="0.7" d="M1 -0.490005L254.53 -230" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M3.02979 18.23L272.5 -226.1" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M11.1699 31.26L288.27 -220.16" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M22.6201 41.22L305.92 -215.97" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M31.5498 53.53L323.72 -211.91" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M42.04 64.38L342.62 -208.88" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M52.4399 75.31L357.08 -201.72" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M62.8701 86.22L374.78 -197.57" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M73.6201 96.83L389.17 -190.35" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M83.1001 108.61L401.16 -180.89" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M93.8701 119.21L414.69 -172.86" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M105.3 129.19L428 -164.64" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M114.93 140.84L426.23 -142.39" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+				</g>
+				<g opacity="0.5">
+					<path opacity="0.7" d="M150.43 129.7L403.97 -99.81" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M152.46 148.41L421.93 -95.91" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M160.6 161.45L437.71 -89.98" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M172.05 171.41L455.35 -85.78" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M180.98 183.71L473.15 -81.73" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M191.47 194.57L492.05 -78.69" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M201.87 205.49L506.51 -71.53" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M212.3 216.41L524.21 -67.38" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M223.05 227.02L538.6 -60.16" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M232.53 238.8L550.59 -50.71" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M243.3 249.4L564.12 -42.68" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M254.73 259.37L577.44 -34.45" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+					<path opacity="0.7" d="M264.36 271.03L575.66 -12.2" stroke="#969696" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+				</g>
+			</g>
+		</svg>
+	</div>
