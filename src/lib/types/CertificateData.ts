2cfe6128ce9146aa3f214ab06920a2e1a7400efe
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-12-04 16:36:56 +0530

    feat: update certificate data model

--- src/lib/types/CertificateData.ts
+++ src/lib/types/CertificateData.ts
@@ -4,22 +4,18 @@ export interface CertificateStaticData {
 	// Certificate details
 	certificateTitle: string;
 	version?: string;
+	validity?: number; // Certificate validity (in years from issueDate)
 	issueDate: Timestamp;
 	startDate: Timestamp;
 	endDate: Timestamp;
 
-	// Certificate validity (in years from issueDate)
-	validity?: number;
-
-	// Issuer details
-	occasion: string; // occasion uniquely identifies a type of certificate
-	description: string;
-
-	role: string; // eg. participant, winner, runners up, organizer, volunteer
-
-	eventName: string;
-	eventID: string;
-	eventDescription: string;
+	// occasion and class identify the certificate
+	occasionType: string; // eg. Hackathon, Workshop, Webinar, Office-bearers etc.
+	occasionID: string; // eg. snh2023, snh2023final etc.
+	occasionName: string;
+	class: string; // eg. participant, winner, runners up, organizer, volunteer etc.
+	position?: string; // if class equals winner, indicates winner, runner up etc.
+	description?: string; // Describes the occasion and class eg. "Hackathon Participant", "Workshop Organizer", etc.
 
 	// We have organization field because we plan to allow multiple organizations to issue certificates in the future
 	organization: string;
@@ -29,9 +25,7 @@ export interface CertificateStaticData {
 	issuerSignatures: string[];
 }
 
-export interface CertificateData extends CertificateStaticData {
-	certificateID: string;
-
+export interface IndividualDetails extends CertificateStaticData {
 	// Recipient details
 	userID: string;
 	name: string;
@@ -43,6 +37,10 @@ export interface CertificateData extends CertificateStaticData {
 	// Team details
 	teamID?: string;
 	teamName?: string;
+}
+
+export interface CertificateData extends IndividualDetails {
+	certificateID: string;
 
 	// Certificate authenticity
 	hash: string;
